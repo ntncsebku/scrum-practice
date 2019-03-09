3c6ebc4b31a2fdbd0d86ddd31f4e027a1e071377
Minh Pham <hacba98@gmail.com>
2019-03-09 10:24:51 +0700

    [API] Update Columns and Items

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,22 +1,24 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 
-const User = require('../../models/User');
-const config = require('../../config');
+const User = require("../../models/User");
+const config = require("../../config");
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    if (!username || username == '') throw new Error('Username is not provided');
-    if (!password || password == '') throw new Error('Password is not provided');
+    if (!username || username == "")
+      throw new Error("Username is not provided");
+    if (!password || password == "")
+      throw new Error("Password is not provided");
   } catch (err) {
     return res.status(422).send({ msg: err.message });
   }
 
-  let user = User.findOne({ username });
-  if (user) return res.status(400).send({ msg: 'Username has been used' });
+  let user = await User.findOne({ username });
+  if (user) return res.status(400).send({ msg: "Username has been used" });
 
   user = new User({ username, password });
   user.save((err, user) => {
@@ -24,34 +26,39 @@ router.post('/register', (req, res) => {
       console.log(err);
       return res.status(500).send({ msg: err.message });
     }
-    res.status(200).json({ msg: 'Register successfully', user });
+    res.status(200).json({ msg: "Register successfully", user });
   });
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
-    if (!username || username == '') throw new Error('Username is not provided');
-    if (!password || password == '') throw new Error('Password is not provided');
+    if (!username || username == "")
+      throw new Error("Username is not provided");
+    if (!password || password == "")
+      throw new Error("Password is not provided");
   } catch (err) {
     return res.status(422).send({ msg: err.message });
   }
 
   const user = await User.findOne({ username });
-  if (!user) return res.status(400).send({ msg: 'User not found' });
+  if (!user) return res.status(400).send({ msg: "User not found" });
   if (user.password !== password) {
-    return res.status(400).send({ msg: 'Password is not correct' });
+    return res.status(400).send({ msg: "Password is not correct" });
   }
 
   user.password = undefined;
 
-  const token = jwt.sign({ data: { username, userId: user._id.toString() } }, config.secretKey, (err, token) => {
-    res.status(200).send({
-      token,
-      user
-    });
-  });
+  const token = jwt.sign(
+    { data: { username, userId: user._id.toString() } },
+    config.secretKey,
+    (err, token) => {
+      res.status(200).send({
+        token,
+        user
+      });
+    }
+  );
 });
-
 
 module.exports = router;
--- server/routes/api/project.js
+++ server/routes/api/project.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 
-const User = require('../../models/User');
-const Project = require('../../models/Project');
+const User = require("../../models/User");
+const Project = require("../../models/Project");
 
-const verifyToken = require('../../middlewares/verifyToken');
+const verifyToken = require("../../middlewares/verifyToken");
 
 const router = express.Router();
 const authRouter = express.Router();
@@ -14,48 +14,53 @@ const authRouter = express.Router();
  * prefix: 'm/'
  */
 authRouter.use(verifyToken);
-router.use('/m', authRouter);
-
+router.use("/m", authRouter);
 
 // Get all projects of an user
-router.get('/user/:username', (req, res) => {
+router.get("/user/:username", (req, res) => {
   const { username } = req.params;
-  User.findOne({ username }, '-password').populate('projects').then((user) => {
-    if (!user) res.status(404).send({ msg: 'User not found' });
-    res.status(200).send(user);
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).send({ msg: err.message });
-  });
+  User.findOne({ username }, "-password")
+    .populate("projects")
+    .then(user => {
+      if (!user) res.status(404).send({ msg: "User not found" });
+      res.status(200).send(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ msg: err.message });
+    });
 });
 
 // Get all my projects
-authRouter.get('/all', (req, res) => {
+authRouter.get("/all", (req, res) => {
   const { username, userId } = req;
-  User.findById(userId, '-password').populate('projects').then((user) => {
-    if (!user) res.status(404).send({ msg: 'User not found' });
-    res.status(200).send(user);
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).send({ msg: err.message });
-  });
+  User.findById(userId, "-password")
+    .populate("projects")
+    .then(user => {
+      if (!user) res.status(404).send({ msg: "User not found" });
+      res.status(200).send(user);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ msg: err.message });
+    });
 });
 
 // Get project data
-router.get('/:projectId', (req, res) => {
+router.get("/:projectId", (req, res) => {
   const { projectId } = req.params;
   Project.findById(projectId, (err, project) => {
     if (err) {
       console.log(err);
       res.status(50).send({ msg: err.message });
     }
-    if (!project) res.status(404).send({ msg: 'Project not found' });
+    if (!project) res.status(404).send({ msg: "Project not found" });
     res.status(200).json(project);
   });
 });
 
 // New project
-authRouter.post('/create', (req, res) => {
+authRouter.post("/create", (req, res) => {
   const { name } = req.body;
   const { username, userId } = req;
   const project = new Project({
@@ -71,89 +76,169 @@ authRouter.post('/create', (req, res) => {
       $addToSet: {
         projects: project._id
       }
-    }).then(() => {
-      res.status(200).json(project);
-    }).catch((err) => {
-      console.log(err);
-      res.status(500).send({ msg: err.message });
-    });
+    })
+      .then(() => {
+        res.status(200).json(project);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).send({ msg: err.message });
+      });
   });
 });
 
-authRouter.post('/:projectId/invite', (req, res) => {
+// add member to project
+authRouter.post("/:projectId/invite", (req, res) => {
   const { projectId } = req.params;
   const { memberId } = req.body;
   const { username, userId } = req;
-  Project.findById(projectId).then((project) => {
-    if (!project) return res.status(404).send({ msg: 'Project not found' });
-    const members = project.members;
-    if (userId != project.creator && !members.find(u => u == userId)) {
-      return res.status(400).send({ msg: 'You dont have permission for this project' });
-    }
-    return Project.findByIdAndUpdate(projectId, {
-      $addToSet: {
-        members: memberId
+  Project.findById(projectId)
+    .then(project => {
+      if (!project) return res.status(404).send({ msg: "Project not found" });
+      const members = project.members;
+      if (userId != project.creator && !members.find(u => u == userId)) {
+        return res
+          .status(400)
+          .send({ msg: "You dont have permission for this project" });
       }
-    }).then(project => User.findByIdAndUpdate(memberId, {
-      $addToSet: {
-        projects: projectId
-      }
-    })).then(() => {
-      res.status(200).send({ msg: 'Invite successfully' });
-    });
-  })
-    .catch((err) => {
+      return Project.findByIdAndUpdate(projectId, {
+        $addToSet: {
+          members: memberId
+        }
+      })
+        .then(project =>
+          User.findByIdAndUpdate(memberId, {
+            $addToSet: {
+              projects: projectId
+            }
+          })
+        )
+        .then(() => {
+          res.status(200).send({ msg: "Invite successfully" });
+        });
+    })
+    .catch(err => {
       console.log(err);
       res.status(500).send({ msg: err.message });
     });
 });
 
 // add column to a project
-authRouter.post('/:projectId/col/add', (req, res) => {
+authRouter.post("/:projectId/col/add", (req, res) => {
   const { projectId } = req.params;
   const { name } = req.body;
   const { username, userId } = req;
-  Project.findById(projectId).then((project) => {
-    if (!project) return res.status(404).send({ msg: 'Project not found' });
-    const members = project.members;
-    if (userId != project.creator && !members.find(u => u == userId)) {
-      return res.status(400).send({ msg: 'You dont have permission for this project' });
-    }
-    project.cols.push({ name });
-    return project.save().then((project) => {
-      res.status(200).send(project);
+  Project.findById(projectId)
+    .then(project => {
+      if (!project) return res.status(404).send({ msg: "Project not found" });
+      const members = project.members;
+      if (userId != project.creator && !members.find(u => u == userId)) {
+        return res
+          .status(400)
+          .send({ msg: "You dont have permission for this project" });
+      }
+      project.cols.push({ name });
+      return project.save().then(project => {
+        res.status(200).send(project);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ msg: err.message });
     });
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).send({ msg: err.message });
-  });
 });
 
 // add item to a column in a project
-authRouter.post('/:projectId/col/:colId/item/add', (req, res) => {
+authRouter.post("/:projectId/col/:colId/item/add", (req, res) => {
   const { projectId, colId } = req.params;
-  const { note, start, due } = req.body;
+  const { title, note, start, due } = req.body;
   const { username, userId } = req;
-  Project.findById(projectId).then((project) => {
-    if (!project) return res.status(404).send({ msg: 'Project not found' });
-    const members = project.members;
-    if (userId != project.creator && !members.find(u => u == userId)) {
-      return res.status(400).send({ msg: 'You dont have permission for this project' });
-    }
-    const colIdx = project.cols.findIndex(u => u._id == colId);
-    if (colId == -1) return res.status(400).send({ msg: 'Column is not exist' });
-    project.cols[colIdx].items.push({
-      note, start, due,
-      author: userId
+  Project.findById(projectId)
+    .then(project => {
+      if (!project) return res.status(404).send({ msg: "Project not found" });
+      const members = project.members;
+      if (userId != project.creator && !members.find(u => u == userId)) {
+        return res
+          .status(400)
+          .send({ msg: "You dont have permission for this project" });
+      }
+      const colIdx = project.cols.findIndex(u => u._id == colId);
+      if (colId == -1)
+        return res.status(400).send({ msg: "Column is not exist" });
+      project.cols[colIdx].items.push({
+        title,
+        note,
+        start,
+        due,
+        author: userId
+      });
+      return project.save().then(() => {
+        res.status(200).send({ msg: "Add item successfully" });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ msg: err.message });
     });
-    return project.save().then(() => {
-      res.status(200).send({ msg: 'Add item successfully' });
-    });
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).send({ msg: err.message });
-  });
 });
 
+// Update comlumn
+authRouter.post("/:projectId/col/:colId/modify", (req, res) => {
+  const { projectId, colId } = req.params;
+  const { name } = req.body;
+  const { username, userId } = req;
+  Project.findById(projectId)
+    .then(project => {
+      if (!project) return res.status(404).send({ msg: "Project not found" });
+      if (userId != project.creator && !members.find(u => u == userId)) {
+        return res
+          .status(400)
+          .send({ msg: "You dont have permission for this project" });
+      }
+      const colIdx = project.cols.findIndex(u => u._id == colId);
+      if (colId == -1)
+        return res.status(400).send({ msg: "Column is not exist" });
+      project.cols[colIdx].name = name;
+      return project.save().then(() => {
+        res.status(200).send({ msg: "Update column name succesfully" });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ msg: err.message });
+    });
+});
+
+// Update card in comlumn
+authRouter.post("/:projectId/col/:colId/item/:itemId/modify", (req, res) => {
+  const { projectId, colId, itemId } = req.params;
+  const { title, note, due, assign } = req.body;
+  Project.findById(projectId)
+    .then(project => {
+      if (!project) return res.status(404).send({ msg: "Project not found" });
+      if (userId != project.creator && !members.find(u => u == userId)) {
+        return res
+          .status(400)
+          .send({ msg: "You dont have permission for this project" });
+      }
+      const colIdx = project.cols.findIndex(u => u._id == colId);
+      if (colId == -1)
+        return res.status(400).send({ msg: "Column is not exist" });
+      const itemIdx = project.cols.items.findIndex(u => u._id == itemId);
+      if (itemIdx == -1)
+        return res.status(400).send({ msg: "Item is not exist." });
+      project.cols[colIdx].items[itemIdx].title = title;
+      project.cols[colIdx].items[itemIdx].note = note;
+      project.cols[colIdx].items[itemIdx].due = due;
+      project.cols[colIdx].items[itemIdx].assign = assign;
+      return project.save().then(() => {
+        res.status(200).send({ msg: "Update card successfully." });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send({ msg: err.message });
+    });
+});
 
 module.exports = router;
